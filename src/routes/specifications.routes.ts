d2559c29eea7f0b4a6f0401575f2dac1b15a8b2b
Felipe Fontana <felipefontana0@gmail.com>
2021-12-29 21:43:59 -0300

    Specifications

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express'
+import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
+import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+
+const specificationsRoutes = Router();
+
+const specificationsREpository = new SpecificationsRepository()
+
+specificationsRoutes.post("/", (request, response) => {
+    const { name, description } = request.body
+    const createSpecificationService = new CreateSpecificationService(specificationsREpository);
+
+    createSpecificationService.execute({ name, description })
+
+    return response.status(201).send()
+})
+
+export { specificationsRoutes }
