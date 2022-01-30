180ffd10a19b2580034283481e28db37bb6e7556
Felipe Fontana <felipefontana0@gmail.com>
2022-01-30 19:01:25 -0300

    Importar arquivo de categorias

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -6,6 +6,9 @@ class ImportCategoryController {
 
     handle(request: Request, response: Response): Response {
         const { file } = request;
+
+        this.importCategoryUseCase.execute(file);
+
         console.log(file)
 
         return response.send()
